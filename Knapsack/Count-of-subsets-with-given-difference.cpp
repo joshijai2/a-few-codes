791a6b162df36dba95442ee0f0a0e7bd0ceebaa3
joshijai2 <joshi.jai232@gmail.com>
2021-03-24 01:22:48 +0530

    add count subset difference

---
+++ Knapsack/Count-of-subsets-with-given-difference.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    //vector <int> val,wt; //uncomment to use vectors instead of array
+    cin >> n;
+    int arr[n];
+    cin >> diff;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    // arr.push_back(x); //uncomment to use vectors instead of array
+
+    sum = (sum - diff) / 2;
+
+    vector<vector<int>> t(n + 1, vector<int>(sum + 1, -1));
+    for (int j = 0; j <= sum; j++)
+        t[0][j] = 0;
+    for (int i = 0; i <= n; i++)
+        t[i][0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= sum; j++)
+        {
+            if (arr[i - 1] <= j)
+                t[i][j] = (t[i - 1][j - arr[i - 1]] + t[i - 1][j]);
+            else
+                t[i][j] = t[i - 1][j];
+        }
+    cout << t[n][sum];
+}
